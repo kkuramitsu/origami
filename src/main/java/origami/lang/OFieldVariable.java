a90dd767177f5465b6f0987525bd692bea7c6065
Kimio Kuramitsu <kimio@ynu.ac.jp>
2017-03-28 18:37:02 +0900

    added OEnvTrait

---
+++ src/main/java/origami/lang/OFieldVariable.java
@@ -0,0 +1,54 @@
+/***********************************************************************
+ * Copyright 2017 Kimio Kuramitsu and ORIGAMI project
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***********************************************************************/
+
+package origami.lang;
+
+import origami.code.OAssignCode;
+import origami.code.OCode;
+import origami.code.OErrorCode;
+import origami.code.ONameCode;
+import origami.lang.type.OType;
+import origami.rule.OFmt;
+
+public class OFieldVariable extends OLocalVariable {
+
+	public OFieldVariable(boolean isReadOnly, String name, OType type) {
+		super(isReadOnly, name, type);
+	}
+
+	public OFieldVariable(String name, OType type) {
+		super(true, name, type);
+	}
+
+	@Override
+	public OCode nameCode(OEnv env, String name) {
+		return new ONameCode(name, this.getType(), this.isReadOnly());
+	}
+
+	@Override
+	public OCode defineCode(OEnv env, OCode right) {
+		return new OAssignCode(this.getType(), true, this.getName(), right);
+	}
+
+	@Override
+	public OCode assignCode(OEnv env, OCode right) {
+		if (this.isReadOnly()) {
+			throw new OErrorCode(env, OFmt.read_only__YY0, this.getName());
+		}
+		return new OAssignCode(this.getType(), false, this.getName(), right);
+	}
+
+}
