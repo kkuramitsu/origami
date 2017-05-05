f3420d981c5adfa71ff9dcd9a88155556b8a7398
Kimio Kuramitsu <kimio@ynu.ac.jp>
2017-05-06 02:27:24 +0900

    updated InlinePass

--- src/main/java/blue/nez/parser/pass/InlinePass.java
+++ src/main/java/blue/nez/parser/pass/InlinePass.java
@@ -41,7 +41,6 @@ import blue.nez.peg.expression.POption;
 import blue.nez.peg.expression.PPair;
 import blue.nez.peg.expression.PRepeat;
 import blue.nez.peg.expression.PRepetition;
-import blue.nez.peg.expression.PValue;
 import blue.nez.peg.expression.PScan;
 import blue.nez.peg.expression.PSymbolAction;
 import blue.nez.peg.expression.PSymbolPredicate;
@@ -49,6 +48,7 @@ import blue.nez.peg.expression.PSymbolScope;
 import blue.nez.peg.expression.PTag;
 import blue.nez.peg.expression.PTrap;
 import blue.nez.peg.expression.PTree;
+import blue.nez.peg.expression.PValue;
 import blue.origami.util.OOption;
 
 public class InlinePass extends CommonPass {
@@ -58,223 +58,237 @@ public class InlinePass extends CommonPass {
 	@Override
 	protected void prepare(Grammar g) {
 		Production start = g.getStartProduction();
-		countMap.put(start.getUniqueName(), 1);
-		count(start.getExpression());
+		this.countMap.put(start.getUniqueName(), 1);
+		this.count(start.getExpression());
 	}
 
 	private void count(Expression e) {
 		if (e instanceof PNonTerminal) {
 			String uname = ((PNonTerminal) e).getUniqueName();
-			Integer n = countMap.get(uname);
+			Integer n = this.countMap.get(uname);
 			if (n == null) {
-				countMap.put(uname, 1);
-				count(((PNonTerminal) e).getProduction().getExpression());
+				this.countMap.put(uname, 1);
+				this.count(((PNonTerminal) e).getProduction().getExpression());
 			} else {
-				countMap.put(uname, n + 1);
+				this.countMap.put(uname, n + 1);
 			}
 		}
 		for (Expression sub : e) {
-			count(sub);
+			this.count(sub);
 		}
 	}
 
 	@Override
 	public Grammar perform(Grammar g, OOption options) {
 		this.options = options;
-		prepare(g);
+		this.prepare(g);
 		for (Production p : g) {
-			if (countMap.get(p.getLocalName()) != null) {
+			if (this.countMap.get(p.getLocalName()) != null) {
 				g.setExpression(p.getLocalName(), this.rewrite(p.getExpression(), null));
 			}
 		}
-		countMap.clear();
-		prepare(g);
+		this.countMap.clear();
+		this.prepare(g);
 		ArrayList<Production> l = new ArrayList<>(g.size());
 		for (Production p : g) {
-			if (countMap.get(p.getLocalName()) != null) {
+			Integer c = this.countMap.get(p.getLocalName());
+			if (c != null) {
+				// System.out.println("remain " + c + ", " + p);
 				l.add(p);
 			}
 		}
+		System.out.printf("inlining %d => %d\n", g.size(), l.size());
+		this.log("inlining %d => %d", g.size(), l.size());
 		g.replaceAll(l);
-		log("inlining %d => %d", g.size(), l.size());
 		return g;
 	}
 
 	@Override
 	public Expression visitNonTerminal(PNonTerminal e, Void a) {
-		Expression deref = Expression.deref(e.getProduction().getExpression());
-		Integer c = countMap.get(e.getUniqueName());
+		Expression p = e.getProduction().getExpression();
+		Expression deref = Expression.deref(p);
+		Integer c = this.countMap.get(e.getUniqueName());
 		if (c == 1) {
-			return optimized(e, deref);
+			return this.optimized(e, deref);
 		}
 		// if (e.getLocalName().isTerminal()) {
 		// return optimized(e, deref);
 		// }
 		// System.out.println("@@ " + nz86.count(deref) + " " + deref);
-		if (nz86.count(deref) < 2) {
-			return optimized(e, deref);
+		if (analysis.cost(deref, 0) <= 2) {
+			// System.out
+			// .println("remain " + c + ", " + analysis.cost(deref, 0) + ", " +
+			// e.getUniqueName() + " = " + deref);
+			return this.optimized(e, deref);
 		}
 		return e;
 	}
 
-	private static NZ86Counter nz86 = new NZ86Counter();
+	private static CostAnalysis analysis = new CostAnalysis();
 
-	private static class NZ86Counter extends ExpressionVisitor<Integer, Integer> {
+	private static class CostAnalysis extends ExpressionVisitor<Integer, Integer> {
 
-		int count(Expression e) {
-			return e.visit(this, 0);
-		}
-
-		private Integer step(Expression e, int n) {
-			return e.get(n).visit(this, 0);
-		}
-
-		@Override
-		public Integer visitNonTerminal(PNonTerminal e, Integer step) {
-			return step + 1;
-		}
-
-		@Override
-		public Integer visitEmpty(PEmpty e, Integer step) {
-			return step;
-		}
-
-		@Override
-		public Integer visitFail(PFail e, Integer step) {
-			return step + 1;
-		}
-
-		@Override
-		public Integer visitByte(PByte e, Integer step) {
-			return step + 1;
-		}
-
-		@Override
-		public Integer visitByteSet(PByteSet e, Integer step) {
-			return step + 1;
-		}
-
-		@Override
-		public Integer visitAny(PAny e, Integer step) {
-			return step + 1;
-		}
-
-		@Override
-		public Integer visitPair(PPair e, Integer step) {
-			if (e.get(0) instanceof PByte) {
-				Expression remaining = Expression.extractMultiBytes(e, null);
-				return remaining.visit(this, step + 1);
+		int cost(Expression e, int calls) {
+			if (calls > 4) {
+				return calls;
 			}
-			return step + step(e, 0) + step(e, 1);
+			return e.visit(this, calls);
 		}
 
 		@Override
-		public Integer visitChoice(PChoice e, Integer step) {
-			int sum = step;
+		public Integer visitNonTerminal(PNonTerminal e, Integer calls) {
+			return calls + 1;
+		}
+
+		@Override
+		public Integer visitEmpty(PEmpty e, Integer calls) {
+			return calls;
+		}
+
+		@Override
+		public Integer visitFail(PFail e, Integer calls) {
+			return calls;
+		}
+
+		@Override
+		public Integer visitByte(PByte e, Integer calls) {
+			return calls + 1;
+		}
+
+		@Override
+		public Integer visitByteSet(PByteSet e, Integer calls) {
+			return calls + 1;
+		}
+
+		@Override
+		public Integer visitAny(PAny e, Integer calls) {
+			return calls + 1;
+		}
+
+		@Override
+		public Integer visitPair(PPair e, Integer calls) {
+			// if (e.get(0) instanceof PByte) {
+			// Expression remaining = Expression.extractMultiBytes(e, null);
+			// return remaining.visit(this, calls + 1);
+			// }
+			return this.cost(e.get(1), this.cost(e.get(0), calls));
+		}
+
+		@Override
+		public Integer visitChoice(PChoice e, Integer calls) {
 			for (Expression sub : e) {
-				sum += sub.visit(this, 2);
+				calls = this.cost(sub, calls + 1);
 			}
-			return sum;
+			return calls;
 		}
 
 		@Override
-		public Integer visitDispatch(PDispatch e, Integer step) {
-			int sum = step;
+		public Integer visitDispatch(PDispatch e, Integer calls) {
 			for (Expression sub : e) {
-				sum += sub.visit(this, 2);
+				calls = this.cost(sub, calls);
 			}
-			return sum;
+			return calls;
 		}
 
 		@Override
-		public Integer visitOption(POption e, Integer step) {
+		public Integer visitOption(POption e, Integer calls) {
 			Expression deref = Expression.deref(e.get(0));
-			Integer in = deref.visit(this, 0);
-			return (in == 1) ? step + 1 : step + in + 2;
+			if (deref instanceof PByte || deref instanceof PByteSet) {
+				return calls + 1;
+			}
+			return this.cost(e.get(0), calls + 1);
 		}
 
 		@Override
-		public Integer visitRepetition(PRepetition e, Integer step) {
+		public Integer visitRepetition(PRepetition e, Integer calls) {
 			Expression deref = Expression.deref(e.get(0));
-			Integer in = deref.visit(this, 0);
-			return (in == 1) ? step + 1 : step + in + 2;
+			if (deref instanceof PByte || deref instanceof PByteSet) {
+				return calls + 1;
+			}
+			return this.cost(e.get(0), calls + 1);
 		}
 
 		@Override
-		public Integer visitAnd(PAnd e, Integer step) {
-			return step(e, 0) + step + 2;
-		}
-
-		@Override
-		public Integer visitNot(PNot e, Integer step) {
+		public Integer visitAnd(PAnd e, Integer calls) {
 			Expression deref = Expression.deref(e.get(0));
-			Integer in = deref.visit(this, 0);
-			return (in == 1) ? step + 1 : step + in + 2;
+			if (deref instanceof PByte || deref instanceof PByteSet) {
+				return calls + 1;
+			}
+			return this.cost(e.get(0), calls + 1);
 		}
 
 		@Override
-		public Integer visitTree(PTree e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitNot(PNot e, Integer calls) {
+			Expression deref = Expression.deref(e.get(0));
+			if (deref instanceof PByte || deref instanceof PByteSet) {
+				return calls + 1;
+			}
+			return this.cost(e.get(0), calls + 1);
 		}
 
 		@Override
-		public Integer visitDetree(PDetree e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitTree(PTree e, Integer calls) {
+			return this.cost(e.get(0), calls + 3);
 		}
 
 		@Override
-		public Integer visitLinkTree(PLinkTree e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitDetree(PDetree e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitTag(PTag e, Integer step) {
-			return step + 1;
+		public Integer visitLinkTree(PLinkTree e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitReplace(PValue e, Integer step) {
-			return step + 1;
+		public Integer visitTag(PTag e, Integer calls) {
+			return calls + 1;
 		}
 
 		@Override
-		public Integer visitSymbolScope(PSymbolScope e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitReplace(PValue e, Integer calls) {
+			return calls + 1;
 		}
 
 		@Override
-		public Integer visitSymbolAction(PSymbolAction e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitSymbolScope(PSymbolScope e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitSymbolPredicate(PSymbolPredicate e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitSymbolAction(PSymbolAction e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitIf(PIfCondition e, Integer step) {
-			return step;
+		public Integer visitSymbolPredicate(PSymbolPredicate e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitOn(POnCondition e, Integer step) {
-			return step;
+		public Integer visitIf(PIfCondition e, Integer calls) {
+			return calls;
 		}
 
 		@Override
-		public Integer visitScan(PScan e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitOn(POnCondition e, Integer calls) {
+			return calls;
 		}
 
 		@Override
-		public Integer visitRepeat(PRepeat e, Integer step) {
-			return step + step(e, 0) + 2;
+		public Integer visitScan(PScan e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
 		}
 
 		@Override
-		public Integer visitTrap(PTrap e, Integer step) {
-			return step + 1;
+		public Integer visitRepeat(PRepeat e, Integer calls) {
+			return this.cost(e.get(0), calls + 2);
+		}
+
+		@Override
+		public Integer visitTrap(PTrap e, Integer calls) {
+			return calls + 1;
 		}
 
 	}
