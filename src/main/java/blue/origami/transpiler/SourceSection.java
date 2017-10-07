0a42f9a19d42f7530c422f7a4e9db615da297f00
Kimio Kuramitsu <kimio@ynu.ac.jp>
2017-10-07 15:57:05 +0900

    removing unused FuncParam

--- src/main/java/blue/origami/transpiler/SourceSection.java
+++ src/main/java/blue/origami/transpiler/SourceSection.java
@@ -336,6 +336,28 @@ abstract class SourceSectionLib implements TCodeSection {
 	}
 }
 
+interface FuncParam {
+
+	public String[] getParamNames();
+
+	public Ty[] getParamTypes();
+
+	public default int getStartIndex() {
+		return 0;
+	}
+
+	public default String getNameAt(int index) {
+		if (getParamNames().length == 0) {
+			return String.valueOf((char) ('a' + index));
+		}
+		return getParamNames()[index] + (this.getStartIndex() + index);
+	}
+
+	public default int size() {
+		return this.getParamTypes().length;
+	}
+}
+
 class Param implements FuncParam, Emitter {
 	final int startIndex;
 	final String[] paramNames;
